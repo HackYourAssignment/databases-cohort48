6ca913d0de438003cca69cd1c9db740d134ff886
Moayad Mohammed <144966859+TheYodash@users.noreply.github.com>
2024-08-20 20:52:01 +0200

    Delete Week2/Assignment/dbConnect.js

--- Week2/Assignment/dbConnect.js
+++
@@ -1,9 +0,0 @@
-// Create connection to the database
-
-import mysql from 'mysql2';
-
-export const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'hyfuser',
-    password: 'hyfpassword',
-});
