c6cea453654db238457e6df2bc8895504e25d76f
Moayad Mohammed <moayad.ashabi@gmail.com>
2024-10-22 01:06:15 +0200

    Refactor aggregate.js queries and close database connection

--- Week2/Assignment/aggregate.js
+++ Week2/Assignment/aggregate.js
@@ -8,13 +8,6 @@ connection.connect((err) => {
     console.log('Connected to the database');
 });
 
-connection.query('USE assignmentDatabase', (err) => {
-    if (err) {
-        console.error('Error using assignment database:', err);
-        return;
-    }
-    console.log('Using assignment database');
-});
 
 
 // All research papers and the number of authors that wrote that paper
@@ -25,14 +18,6 @@ SELECT rp.paper_title, COUNT(arp.author_id) AS num_authors
     GROUP BY rp.paper_id;
     `;
 
-connection.query(query1, (err, results) => {
-    if (err) {
-        console.error('Error retrieving research papers:', err);
-        return;
-    }
-    console.log('Research papers and number of authors:', results);
-});
-
 // Sum of the research papers published by all female authors
 const query2 = `
 SELECT COUNT(rp.paper_id) AS total_papers
@@ -41,27 +26,11 @@ SELECT COUNT(rp.paper_id) AS total_papers
     INNER JOIN authors a ON arp.author_id = a.author_id
     WHERE a.gender = 'F';`;
 
-connection.query(query2, (err, results) => {
-    if (err) {
-        console.error('Error retrieving sum of research papers:', err);
-        return;
-    }
-    console.log('Sum of research papers published by female authors:', results);
-});
-
 // Average of the h-index of all authors per university
 const query3 = `
 SELECT university, AVG(h_index) AS avg_h_index
     FROM authors
     GROUP BY university;`;
-
-connection.query(query3, (err, results) => {
-    if (err) {
-        console.error('Error retrieving average h-index:', err);
-        return;
-    }
-    console.log('Average h-index of authors per university:', results);
-});
 
 // Sum of the research papers of the authors per university
 const query4 = `
@@ -70,26 +39,64 @@ SELECT university, COUNT(arp.paper_id) AS total_papers
     INNER JOIN authors a ON arp.author_id = a.author_id
     GROUP BY university;`;
 
-connection.query(query4, (err, results) => {
-    if (err) {
-        console.error('Error retrieving sum of research papers:', err);
-        return;
-    }
-    console.log('Sum of research papers of authors per university:', results);
-});
-
 // Minimum and maximum of the h-index of all authors per university
 const query5 = `
 SELECT university, MIN(h_index) AS min_h_index, MAX(h_index) AS max_h_index
     FROM authors
     GROUP BY university;`;
 
-connection.query(query5, (err, results) => {
+connection.query('USE assignmentDatabase', (err) => {
     if (err) {
-        console.error('Error retrieving min and max h-index:', err);
+        console.error('Error using assignment database:', err);
         return;
     }
-    console.log('Min and max h-index of authors per university:', results);
+    console.log('Using assignment database');
+    try {
+    connection.query(query1, (err, results) => {
+        if (err) {
+            console.error('Error retrieving research papers:', err);
+            return;
+        }
+        console.log('Research papers and number of authors:', results);
+    });
+    connection.query(query2, (err, results) => {
+        if (err) {
+            console.error('Error retrieving sum of research papers:', err);
+            return;
+        }
+        console.log('Sum of research papers published by female authors:', results);
+    });
+    connection.query(query3, (err, results) => {
+        if (err) {
+            console.error('Error retrieving average h-index:', err);
+            return;
+        }
+        console.log('Average h-index of authors per university:', results);
+    });
+    connection.query(query4, (err, results) => {
+        if (err) {
+            console.error('Error retrieving sum of research papers:', err);
+            return;
+        }
+        console.log('Sum of research papers of authors per university:', results);
+    });
+    connection.query(query5, (err, results) => {
+        if (err) {
+            console.error('Error retrieving min and max h-index:', err);
+            return;
+        }
+        console.log('Min and max h-index of authors per university:', results);
+    });
+    } catch (err) {
+        console.error('Error running queries:', err);
+    } finally {
+        closeConnection();
+    }    
+});
+
+
+// close the connection
+const closeConnection = () => {
     connection.end((err) => {
         if (err) {
             console.error('Error disconnecting from the database:', err);
@@ -97,4 +104,4 @@ connection.query(query5, (err, results) => {
         }
         console.log('Disconnected from the database');
     });
-});
+}
